946245bc2cea162e262b483ea5cc399ffe1ab2e5
sm3ip <lucasomenpc@gmail.com>
2023-12-27 13:54:04 +0100

    score's done

---
+++ src/Co4Score.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class Score():
+    def __init__(self, size):
+        self.size = size
+        self.scores = np.arange(self.size, dtype=int)
+        self.scores.fill(0)
+
+    def updateScore(self,player, score):
+        if 0 <= player <= self.size:
+            self.scores[player] += score
+            return True
+        else:
+            return False
+
+    def getScore(self):
+        tempMess = "The current score is  : "
+        for i in range(len(self.scores)):
+            tempMess += "\n {} : ".format(self.scores[i])
+        return tempMess
