379c3f72347419c3dd6d5ecc01aeea22e7264f4b
sm3ip <lucasomenpc@gmail.com>
2023-12-28 18:34:01 +0100

    gameboard comments all done

--- src/Co4Gameboard.py
+++ src/Co4Gameboard.py
@@ -2,88 +2,129 @@ import numpy as np
 
 
 class Gameboard:
-    def __init__(self, turn): #turn is an int choosing which player goes first
+    # -*- coding: utf-8 -*-
+    """
+    A class representing a Connect 4 gameBoard
+    Attributes:
+        board (numpy 2-D array): the gameboard
+        playCount (int): the number of turns taken; to check for stalemate (stalemate is at 6*7=42)
+        winner (int): the id of this game's winner; -1 is no one, 0 is P1, 1 is P2
+        turn (int): the id of this game's turn; 0 is P1, 1 is P2
+        name (str): the name of the game
+    Methods:
+        __init__: initialize the gameBoard
+        takeTurn: simulates a turn and returns true if it happened (i.e. if it was possible)
+        checkVictory: checks if the previous move landed a victory
+        showBoard: prints the gameboard in a pretty format
+        play: The game loop
+    """
+    def __init__(self, turn):
+        # -*- coding: utf-8 -*-
+        """ Builds the gameBoard
+
+        :param turn: an int deciding on which player goes first
+        """
         self.board = np.full((6, 7), -1, dtype=int) #sets the board at its original value
-        self.playCount = 0 # to check for stalemate (stalemate is at 6*7=42)
-        self.winner = -1 # -1 is no one, 0 is P1, 1 is P2
-        self.turn = turn # which player's turn it is
+        self.playCount = 0
+        self.winner = -1
+        self.turn = turn
         self.name = "Connect 4"
 
     def takeTurn(self, column):
+        # -*- coding: utf-8 -*-
+        """ Simulates a turn on a given column
+
+        :param column: an int deciding on where the player wants to place their token
+        :return: true if the turn was possible, false otherwise
+        """
         if column<0 or column>6 or self.board[0][column] != -1:
             return False
         else:
             isDone = False
             for i in range(5,-1,-1):
+                #check for the first empty spot in this particuliar column
                 if self.board[i][column]==-1 and not isDone:
                     self.board[i][column] = self.turn
                     isDone = True
                     if self.checkVictory(column,i):
+                        # when found check if this move was a decisive one
                         self.winner = self.turn
             return True
-        #this one is a simple turn returns false if the column is full
-        #true otherwise but checks where to place and checks victory on that particuliar spot
 
     def checkVictory(self, column,row):
-        #from a particuliar spot there's less checks to do
+        # -*- coding: utf-8 -*-
+        """ Checks if there are 4 tokens connected to a given spot (the winning condition of this game)
+
+        :param column: an int representing the x-axis of the spot
+        :param row: an int representing the y-axis of the spot
+        :return: true if the spot is part of a line of 4 tokens (of same value), false otherwise
+        """
         tempC = column
         tempR = row
-        #gotta check from left to right
+        # Check from left to right
         while 1<=tempC<=6 and self.board[tempR][tempC-1]==self.turn:
-            tempC-=1 #gets to the furthest left
+            tempC-=1 # gets to the furthest left
         streak = 1
         while 0<=tempC<=5 and self.board[tempR][tempC+1]==self.turn:
-            streak += 1
+            streak += 1 # goes all the way to the right
             tempC+=1
         if streak>=4:
             return True
-        #then check from top to bottom
+
+        # Check from top to bottom
         while 1<=tempR<=5 and self.board[tempR-1][tempC]==self.turn:
-            tempR-=1 #gets to the furthest left
+            tempR-=1 #gets to the top-most spot with the same token value
         streak = 1
         while 0<=tempR<=4 and self.board[tempR+1][tempC]==self.turn:
-            streak += 1
+            streak += 1 # goes all the way to the bottom
             tempR+=1
         if streak>=4:
             return True
 
-        #then check from top left to bottom right
+        # Check from top left to bottom right
         while 1<=tempC<=6 and 1<=tempR<=5 and self.board[tempR-1][tempC-1]==self.turn:
-            tempR-=1 #gets to the furthest left
+            tempR-=1 # gets to the top-left spot with the same token value
             tempC -= 1
         streak = 1
         while 0<=tempC<=5 and 0<=tempR<=4 and self.board[tempR+1][tempC+1]==self.turn:
-            streak += 1
+            streak += 1 # goes all the way to the bottom-right
             tempR+=1
             tempC+=1
         if streak>=4:
             return True
 
-        #then check from bottom left to top right
-
+        # Check from bottom left to top right
         while 1<=tempC<=6 and 0<=tempR<=4 and self.board[tempR+1][tempC-1]==self.turn:
-            tempR+=1 #gets to the furthest left
+            tempR+=1 # gets to the bottom-left spot with the same token value
             tempC -= 1
         streak = 1
         while 0<=tempC<=5 and 1<=tempR<=5 and self.board[tempR-1][tempC+1]==self.turn:
-            streak += 1
+            streak += 1 # goes all the way to the top-right
             tempR-=1
             tempC+=1
         if streak>=4:
             return True
 
-        return False
+        return False # if no victory condition found
 
     def showBoard(self):
-        # gotta print more information about the current turn
+        # -*- coding: utf-8 -*-
+        """
+        Displays the board with all the important information
+        """
         print("Next player to play is P", self.turn + 1)
         print("Play count :", self.playCount)
         print(self.board)
 
     def play(self):
+        # -*- coding: utf-8 -*-
+        """
+        Simulates the game loop
+        """
         while self.playCount <43 and self.winner == -1:
             isPlacementGood = False
             while not isPlacementGood:
+                # take a turn to a given column only if possible
                 print("In which column do you wanna play ?")
                 column = int(input())
                 if self.takeTurn(column):
